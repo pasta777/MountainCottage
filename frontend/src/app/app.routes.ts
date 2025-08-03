bd0a21dd15809b7686bfada8a3aff640dbd1078a
Stepan Ignjatovic <axegamerssrb@gmail.com>
2025-08-03 16:20:56 +0200

    feat: Add admin login and dashboard to route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { Register } from './pages/register/register';
+import { AdminLogin } from './pages/admin-login/admin-login';
+import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
 
 export const routes: Routes = [
     {path: 'register', component: Register},
+    {path: 'admin/login', component: AdminLogin},
+    {path: 'admin/dashboard', component: AdminDashboard}
 ];
