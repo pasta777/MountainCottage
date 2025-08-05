347d669aa6e560e0a973858f2350a69bc5f63f8c
Stepan Ignjatovic <axegamerssrb@gmail.com>
2025-08-05 21:03:22 +0200

    fix: Revert custom icon

--- frontend/src/app/pages/cottage-details/cottage-details.ts
+++ frontend/src/app/pages/cottage-details/cottage-details.ts
@@ -94,8 +94,18 @@ export class CottageDetails implements OnInit {
 
     const coords = L.latLng(this.cottage.coordinates.lat, this.cottage.coordinates.lon);
 
+    const customIcon = L.icon({
+      iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+      iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+      shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+
     this.map.setView(coords, 13);
-    L.marker(coords).addTo(this.map).bindPopup(this.cottage.name);
+    L.marker(coords, { icon: customIcon }).addTo(this.map).bindPopup(this.cottage.name);
   }
 
   nextStep(): void {
